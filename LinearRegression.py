ea302f6985d3b23e9f42119e5643cb2b85457395
Alex Katopodis <aak61@duke.edu>
2022-09-30 18:53:16 -0400

    Fit via Normal Equation

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,3 +1,30 @@
 """
 Standard Linear Regression.
 """
+import numpy as np
+
+
+class LinearRegression(object):
+    def __init__(self):
+        self.theta = np.array([])
+
+    def fit(self, X, y, method="normal"):
+        if method == "normal":
+            self.fit_normal(X, y)
+
+    def fit_normal(self, X, y):
+        self.theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+
+    def predict(self, X):
+        return X.T.dot(self.theta)
+
+    def show_model(self):
+        print(self.theta)
+
+    # TODO: save the model to a file for later use without retraining
+    def save_model(self):
+        pass
+
+    # TODO: import a model from a save file to avoid retraining
+    def import_model(self, model_name):
+        pass
